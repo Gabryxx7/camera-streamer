d7ebd9bffe4ddd0c9976f7ec9c7dadabd1826ee6
gabryxx7 <giaga7@gmail.com>
2022-07-20 06:27:23 +0000

    Added show more/less

--- react-anthro/create-react-app/src/collaborators/Collaborators.js
+++ react-anthro/create-react-app/src/collaborators/Collaborators.js
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react';
+import React, { Component, useState }  from 'react';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack'
 import Box from '@mui/material/Box';
@@ -9,22 +9,40 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
+import Collapse from '@mui/material/Collapse';
+import ExpandLessOutlinedIcon from '@mui/icons-material/ExpandLessOutlined';
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+
 
 function CollaboratorCard(props){
+    const collapsible = props.collapsible === undefined ? true : props.collapsible;
+    const collapseSize = props.collapseSize || "30rem";
+    const [collapsed, setCollapsed] = useState(collapsible);
+  
+    const handleChange = () => {
+        setCollapsed((prev) => !prev);
+    };
+  
     const image = props.image || null;
-    const name = props.name || "Jane Doe";
-    const url = props.url || "";
+    const name = props.name || null;
+    const url = props.url || null;
     const urlText = props.urlText || url;
     const direction = props.direction || 'row';
     const xs = props.span || 3
     let imgHeight = direction === 'row' ? '100%' : '100%'
     imgHeight = props.imgSize === 'sm' ? '20%' : imgHeight
-    let imgWidth =  props.imgSize === 'sm' ? '60%' : '100%'
-    const padding = direction === 'row' ? '0rem 2.5rem' : '2rem 0rem'
+    let imgWidth =  props.imgSize === 'sm' ? '80%' : '100%'
+    let padding = props.padding || null;
+    let textMarginTop = '0'
+    if(padding === null){
+        padding = direction === 'row' ? '0rem 2.5rem' : '2rem 0rem'
+        textMarginTop = '1.5rem'
+    }
     const nameSize = direction === 'row' ? '3rem' : '1.8rem'
     const textWidth = direction === 'row' ? '75%' : '100%'
     const textSize = direction === 'row' ? '1.3rem' : '1.1rem'
     const lineHeight = direction === 'row' ? '1.6rem' : '1.25rem'
+    console.log(name +": " +collapsible);
     return(
         <Grid item xs={xs}>
             <Card
@@ -36,7 +54,8 @@ function CollaboratorCard(props){
                     alignItems: 'center',
                     justifyItems: 'center',
                     background:'transparent',
-                    boxShadow: 'unset' }}>
+                    boxShadow: 'unset',
+                    overflow: "visible" }}>
                 {image != null ?
                     <Box 
                      display='flex'
@@ -62,13 +81,57 @@ function CollaboratorCard(props){
                 }
                
                 <CardContent sx={{ flexGrow: 0, padding: padding, width:textWidth }}>
+
+                {name != null ?
                 <Typography variant="h5" fontSize={nameSize} align="left" sx={{marginBottom:"0", lineHeight:"1"}}>
                     {name}
-                </Typography>
-                <Link href={url} color="primary" fontSize="14pt">{urlText}</Link>
-                <Typography  component='div' display="block" fontSize={textSize} align="left" lineHeight={lineHeight} sx={{marginTop:"1.5rem", overflowWrap: 'anywhere'}}
+                </Typography> : ""}
+                
+
+                {url != null ? <Link href={url} color="primary" fontSize="14pt">{urlText}</Link> : ""}
+                
+                <Typography  component='div' display="block" fontSize={textSize} align="left" lineHeight={lineHeight}
+                sx={{
+                    marginTop:textMarginTop,
+                    overflowWrap: 'anywhere',
+                    position: 'relative',
+                    paddingBottom: '1rem',
+                    overflow: "visible"}}
                     nowrap='false'>
-                    {props.children}
+                    {collapsible ?
+                        <div>
+                            <Collapse orientation='vertical' collapsedSize={collapseSize} in={!collapsed}>
+                                {props.children}
+                            </Collapse>
+                            <Box
+                            className="expandCollabText" onClick={handleChange}
+                            height={collapsed ? '100%' : '10%'}
+                            bottom={collapsed ? '0' : '-10%'}
+                            sx={{
+                                display: 'flex',
+                                gap:"0px",
+                                alignItems: 'space-around',
+                                alignContent: 'space-around',
+                                justifyItems: 'space-around',
+                                justifyContent: 'end',
+                                flexDirection: 'column',
+                                textAlign: 'center',
+                                zIndex: '500',
+                                width: '100%',
+                                position: 'absolute',
+                                padding: '0rem 0rem',
+                                background: 'rgb(18,18,18)',
+                                background: 'linear-gradient(0deg, rgba(18,18,18,1) 0%, rgba(18,18,18,0.8105061848958334) 42%, rgba(0,0,0,0) 100%)',
+                                fontWeight: '800',
+                                fontSize: '13pt',
+                            }}>
+                                <div>{collapsed ? "Show More" : "Show Less"}</div>
+                                <div>
+                                    {collapsed ? <ExpandMoreOutlinedIcon fontSize="medium" /> : <ExpandLessOutlinedIcon fontSize="medium" /> }
+                                </div>
+                            </Box>
+                        </div>
+                    : props.children}
                 </Typography>
                 </CardContent>
             </Card>
@@ -101,8 +164,9 @@ export default function Collaborators(props) {
             alignItems="space-around"
             alignContent="space-around"
             rowGap={12}
+            columnGap={1}
             direction="row">
-            <CollaboratorCard direction='row' span={12} image={require("./media/stelarc.png")} name="Stelarc" url="http://stelarc.org/" urlText="http://stelarc.org/">
+            <CollaboratorCard collapsible={false} direction='row' span={12} image={require("./media/stelarc.png")} name="Stelarc" url="http://stelarc.org/" urlText="http://stelarc.org/">
                 Stelarc is a performance artist who has visually probed and acoustically amplified his body.
                 He has made three films of the inside of his body. Between 1976-1988 he completed 26 body suspension performances with hooks into the skin.
                 He has used medical instruments, prosthetics, robotics, Virtual Reality systems, the Internet and biotechnology to engineer intimate and involuntary interfaces with the body. He explores Alternate Anatomical Architectures with augmented and extended body constructs.
@@ -111,6 +175,27 @@ export default function Collaborators(props) {
                 His FRACTAL FLESH, PING BODY and PARASITE performances explored involuntary, remote and internet choreography of the body with electrical stimulation of the muscles.
                 His PROSTHETIC HEAD is an embodied conversational agent that speaks to the person who interrogates it. He is surgically constructing an EXTRA EAR on his arm that will be internet enabled, making it a publicly accessible acoustical organ for people in other places. He is presently performing as his avatar from his SECOND LIFE site.
             </CollaboratorCard>
+            <Grid
+                height="100%"
+                width="100%"
+                align="left"
+                container
+                justifyItems="space-evenly"
+                justifyContent="space-evenly"
+                alignItems="space-evenly"
+                alignContent="space-evenly"
+                rowGap={12}
+                columnGap={1}
+                direction="row">
+                <CollaboratorCard direction='column' span={3} image={require("./media/paulloh.png")} name="Dr Paul Loh (MSD)"
+            url="Link to personal website" urlText="Link to personal website">                
+                Dr Paul Loh is Senior Lecturer in digital architecture design and co-director of the Advanced Digital Design and Fabrication (ADD+F)  research hub at the Melbourne School of Design, University of Melbourne.
+                He studied architecture at the University of Melbourne, the University of East London (UEL), the Architectural Association (Design Research Lab) and gained his doctorate at RMIT University.
+                Paul previously taught at UEL and the AA and had lectured at Lund University, ETH Zurich, and Tsinghua University.
+                <br/><br/>
+                His research focuses on the cognitive engagement of spatial, material and narrative making through digital media from robotics to virtual reality. Paul is a registered architect in the UK, where his built projects have been widely published and have received numerous awards. His drawings and art objects have been exhibited and represented at the Royal Academy in London, the London Design Festival and London Frieze Art Fair. 
+                He is founding partner of Melbourne based architecture practice LLDS / Power to Make, focusing on the relationship between making, technology and material. <Link href="https://findanexpert.unimelb.edu.au/profile/491353-paul-loh" color="primary">https://findanexpert.unimelb.edu.au/profile/491353-paul-loh</Link>
+            </CollaboratorCard>
              <CollaboratorCard direction='column' span={3} image={require("./media/davidleggett.png")} name="David Leggett (LLDS)" url="Link to personal website" urlText="Link to personal website" >
                 David Leggett is founding partner of LLDS / Power to Make, a Melbourne based practice that integrates CNC robotic fabrication with architectural design.
                 He studied architecture at the University of East London and the University of Westminster. He worked with Edward Cullinan Architects as Director for over ten years before establishing LLDS / Power to Make in 2011. 
@@ -118,30 +203,35 @@ export default function Collaborators(props) {
                 His built projects include the Bristol Harbourside Masterplan, Singapore Management University, the International Digital Laboratory for the University of Warwick and the Master Film Store for the British Film Institute.
                 David has taught Master in Architecture at the University of Melbourne since 2012. He has lectured at the University of Lund and has conducted design workshops at the University of Tsinghua, Beijing. <Link href="www.llds.com.au" color="primary">www.llds.com.au</Link>
              </CollaboratorCard>
-            <CollaboratorCard direction='column' span={3} image={require("./media/paulloh.png")} name="Dr Paul Loh (MSD)"
-            url="Link to personal website" urlText="Link to personal website">
-                Dr Paul Loh is Senior Lecturer in digital architecture design and co-director of the Advanced Digital Design and Fabrication (ADD+F)  research hub at the Melbourne School of Design, University of Melbourne.
-                He studied architecture at the University of Melbourne, the University of East London (UEL), the Architectural Association (Design Research Lab) and gained his doctorate at RMIT University.
-                Paul previously taught at UEL and the AA and had lectured at Lund University, ETH Zurich, and Tsinghua University.
-                <br/><br/>
-                His research focuses on the cognitive engagement of spatial, material and narrative making through digital media from robotics to virtual reality. Paul is a registered architect in the UK, where his built projects have been widely published and have received numerous awards. His drawings and art objects have been exhibited and represented at the Royal Academy in London, the London Design Festival and London Frieze Art Fair. 
-                He is founding partner of Melbourne based architecture practice LLDS / Power to Make, focusing on the relationship between making, technology and material. <Link href="https://findanexpert.unimelb.edu.au/profile/491353-paul-loh" color="primary">https://findanexpert.unimelb.edu.au/profile/491353-paul-loh</Link>
-                
-            </CollaboratorCard>
-            <CollaboratorCard direction='column' span={5} name="HCI Group" url="https://cis.unimelb.edu.au/hci"  urlText="Human-Computer Interaction Lab" >
-                <CollaboratorCard direction='column' span={12} image={require("./media/stelarc.png")} imgSize="sm"
+            
+            <CollaboratorCard direction='column' span={3} padding='0' collapsible={false} >
+                <CollaboratorCard direction='column' span={12} image={require("./media/stelarc.png")} imgSize="lg" collapseSize="8rem"
                     name="Qiushi Zhou" url="https://qiushi-zhou.github.io/" urlText="https://qiushi-zhou.github.io/">
                     Stelarc is a performance artist who has visually probed and acoustically amplified his body.
                     He has made three films of the inside of his body. Between 1976-1988 he completed 26 body suspension performances with hooks into the skin.
                     He has used medical instruments, prosthetics, robotics, Virtual Reality systems, the Internet and biotechnology to engineer intimate and involuntary interfaces with the body. He explores Alternate Anatomical Architectures with augmented and extended body constructs.
                     </CollaboratorCard>
-                <CollaboratorCard direction='column' span={12} image={require("./media/stelarc.png")} imgSize="sm"
+                <CollaboratorCard direction='column' span={12} image={require("./media/stelarc.png")} imgSize="lg" collapseSize="8rem"
                     name="Gabriele Marini" url="https://gmarini.com/" urlText="https://gmarini.com/">
                     Stelarc is a performance artist who has visually probed and acoustically amplified his body.
                     He has made three films of the inside of his body. Between 1976-1988 he completed 26 body suspension performances with hooks into the skin.
                     He has used medical instruments, prosthetics, robotics, Virtual Reality systems, the Internet and biotechnology to engineer intimate and involuntary interfaces with the body. He explores Alternate Anatomical Architectures with augmented and extended body constructs.
                     </CollaboratorCard>
                 </CollaboratorCard>
+
+                {/* <CollaboratorCard direction='column' span={3} image={require("./media/stelarc.png")} imgSize="sm"
+                    name="Qiushi Zhou" url="https://qiushi-zhou.github.io/" urlText="https://qiushi-zhou.github.io/">
+                    Stelarc is a performance artist who has visually probed and acoustically amplified his body.
+                    He has made three films of the inside of his body. Between 1976-1988 he completed 26 body suspension performances with hooks into the skin.
+                    He has used medical instruments, prosthetics, robotics, Virtual Reality systems, the Internet and biotechnology to engineer intimate and involuntary interfaces with the body. He explores Alternate Anatomical Architectures with augmented and extended body constructs.
+                    </CollaboratorCard>
+                <CollaboratorCard direction='column' span={3} image={require("./media/stelarc.png")} imgSize="sm"
+                    name="Gabriele Marini" url="https://gmarini.com/" urlText="https://gmarini.com/">
+                    Stelarc is a performance artist who has visually probed and acoustically amplified his body.
+                    He has made three films of the inside of his body. Between 1976-1988 he completed 26 body suspension performances with hooks into the skin.
+                    He has used medical instruments, prosthetics, robotics, Virtual Reality systems, the Internet and biotechnology to engineer intimate and involuntary interfaces with the body. He explores Alternate Anatomical Architectures with augmented and extended body constructs.
+                    </CollaboratorCard> */}
+                    </Grid>
         </Grid>
     </Stack>
     );
